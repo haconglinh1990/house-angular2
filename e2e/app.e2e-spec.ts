2b63d5df5b4ae0f80feae340bb2b6c3b9612416d
angular-cli <angular-cli@angular.io>
2016-11-09 14:54:44 +0700

    chore: initial commit from angular-cli                           _                      _ _    __ _ _ __   __ _ _   _| | __ _ _ __       ___| (_)   / _  |  _ \ / _  | | | | |/ _  |  __|____ / __| | |  | (_| | | | | (_| | |_| | | (_| | | |_____| (__| | |   \____|_| |_|\__  |\____|_|\____|_|        \___|_|_|               |___/

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { HouseMaterialPage } from './app.po';
+
+describe('house-material App', function() {
+  let page: HouseMaterialPage;
+
+  beforeEach(() => {
+    page = new HouseMaterialPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
